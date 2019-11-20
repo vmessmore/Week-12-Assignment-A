ddea7dbbe133882f86e5434fe0b18e808654c2ae
Victoria Messmore <vmessmore@mymail.mines.edu>
2019-11-19 20:53:40 -0700

    Added UpdateString function

--- Week-12-Utility.py
+++ Week-12-Utility.py
@@ -6,3 +6,12 @@ def LoadFile(file_name):
     file = open(file_name, 'r', encoding='utf-8-sig')
     file_lines = file.read().splitlines()
     return file_lines
+
+def UpdateString(original, edit, number):
+    new_string=''
+    for i in range(len(original)):
+        if i == number:
+            new_string += edit
+        else:
+            new_string += original[i]
+    PrintOutput(new_string)
