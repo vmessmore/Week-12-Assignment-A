2aa16fc6f0caef8a62e22a1bbc8e75010d8f5cea
Victoria Messmore <vmessmore@mymail.mines.edu>
2019-11-19 20:47:31 -0700

    LoadFile Function added

--- Week-12-Utility.py
+++ Week-12-Utility.py
@@ -1,2 +1,8 @@
 def PrintOutput(the_string):
     print('OUTPUT', the_string)
+
+def LoadFile(file_name):
+    lines = []
+    file = open(file_name, 'r', encoding='utf-8-sig')
+    file_lines = file.read().splitlines()
+    return file_lines
